4c988bc52ae852557a4c7572d6231f4598e6da57
Kaê Uchôa <kaeuchoa@hotmail.com>
2020-09-17 09:27:02 -0300

    adds quotes to dummy data array

--- index.js
+++ index.js
@@ -12,7 +12,16 @@ app.use(cors());
 //Public route
 app.get('/api/quotes/public', (req, res) => {
     let quotes = [
-        
+        {
+            id: 123,
+            body: "Sed porttitor lectus nibh. Proin eget tortor risus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.",
+            user: {
+                id: 123,
+                age: 20
+            },
+            upVotes: 0,
+            toAge: 13
+        }
     ];
     res.json(quotes);
 });
