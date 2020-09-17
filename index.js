17b1fb3e8453e6e2cfa3bf3a405acdeeb9dc1b85
Kaê Uchôa <kaeuchoa@hotmail.com>
2020-09-17 09:28:03 -0300

    adds quotes to dummy data array

--- index.js
+++ index.js
@@ -21,7 +21,27 @@ app.get('/api/quotes/public', (req, res) => {
             },
             upVotes: 0,
             toAge: 13
-        }
+        },
+        {
+            id: 124,
+            body: "Sed porttitor lectus nibh. Proin eget tortor risus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.",
+            user: {
+                id: 123,
+                age: 20
+            },
+            upVotes: 0,
+            toAge: 13
+        },
+        {
+            id: 125,
+            body: "Sed porttitor lectus nibh. Proin eget tortor risus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.",
+            user: {
+                id: 123,
+                age: 20
+            },
+            upVotes: 0,
+            toAge: 13
+        },
     ];
     res.json(quotes);
 });
@@ -29,7 +49,37 @@ app.get('/api/quotes/public', (req, res) => {
 // private route
 app.get('/api/quotes/private', (req, res) => {
     let quotes = [
-        // array of public quotes here
+        // array of private quotes here
+        {
+            id: 234,
+            body: "Sed porttitor lectus nibh. Proin eget tortor risus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.",
+            user: {
+                id: 125,
+                age: 20
+            },
+            upVotes: 0,
+            toAge: 13
+        },
+        {
+            id: 235,
+            body: "Sed porttitor lectus nibh. Proin eget tortor risus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.",
+            user: {
+                id: 125,
+                age: 20
+            },
+            upVotes: 0,
+            toAge: 13
+        },
+        {
+            id: 236,
+            body: "Sed porttitor lectus nibh. Proin eget tortor risus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.",
+            user: {
+                id: 125,
+                age: 20
+            },
+            upVotes: 0,
+            toAge: 13
+        },
     ];
     res.json(quotes);
 });
