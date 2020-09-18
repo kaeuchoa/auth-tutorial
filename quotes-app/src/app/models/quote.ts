d6c4cfdfd97e5d2367e388b4c49e5b6c5bd6d1de
Kaê Uchôa <kaeuchoa@hotmail.com>
2020-09-18 09:02:38 -0300

    adds code to models

--- quotes-app/src/app/models/quote.ts
+++ quotes-app/src/app/models/quote.ts
@@ -1,2 +1,19 @@
+// {
+//     id: 123,
+//     body: "Sed porttitor lectus nibh. Proin eget tortor risus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus.",
+//     user: {
+//         id: 123,
+//         age: 20
+//     },
+//     upVotes: 0,
+//     toAge: 13
+// },
+import { User } from "./user";
+
 export class Quote {
+    id: number;
+    body: string;
+    user: User;
+    upVotes: number;
+    toAge: number;
 }
