ba6c096ec3c5947c7d660c8f076ca93e92bac0ad
Kaê Uchôa <kaeuchoa@hotmail.com>
2020-09-18 08:58:00 -0300

    adds paths to routing module

--- quotes-app/src/app/app-routing.module.ts
+++ quotes-app/src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CallbackComponent } from './components/callback.component';
+import { PrivateQuotesComponent } from './components/private-quotes/private-quotes.component';
+import { PublicQuotesComponent } from './components/public-quotes/public-quotes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'quotes',
+        pathMatch: 'full'
+    },
+    {
+        path: 'quotes',
+        component: PublicQuotesComponent
+    },
+    {
+        path: 'special',
+        component: PrivateQuotesComponent
+    },
+    {
+        path: 'callback',
+        component: CallbackComponent
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
